bf98a5aa3688dd630199f157dbb413371db9451f
dangtung <language0512@gmail.com>
2023-01-31 19:21:03 +0700

    update post module

---
+++ src/post/post.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Query, Param, Body, Req, Put, Post } from '@nestjs/common';
+import { PostService } from './post.service';
+
+@Controller('post')
+export class PostController {
+    constructor(private readonly postService: PostService) {}
+
+    @Get()
+    getAllPost(@Query() { } : {}) {
+    }
+
+    @Get()
+    getPostById(@Param('id') id: string) {
+    }
+
+    @Post()
+    async createPost(@Req() req: any, @Body() post: {}) {
+    }
+
+    @Put(':id')
+    async replacePost(@Param('id') id: string, @Body() post: {} ) {
+    }
+}
